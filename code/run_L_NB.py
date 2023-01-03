05aff6c07c855059c31a0f58e9a232929f6b000a
Felipe Gómez <fegomezl@unal.edu.co>
2023-01-03 09:40:39 -0500

    fixed thermodinamic limit script

--- code/run_L_NB.py
+++ code/run_L_NB.py
@@ -8,7 +8,9 @@ import misc
 with open('settings/run_L_NB.yml', 'r') as f:
     parameters = yaml.safe_load(f)
 parameters['L'] = int(sys.argv[1])
-parameters['N_B'] = int(sys.argv[2])
+parameters['N_FU'] = int(sys.argv[2])
+parameters['N_FD'] = int(sys.argv[3])
+parameters['N_B'] = int(sys.argv[4])
 sim_params = misc.read_settings(parameters)
 sim_params['log_params']['filename'] = 'results/'+misc.create_foldername(parameters)+'/'+misc.create_filename(parameters)+'.log'
 
--- scripts/run_L_NB.sh
+++ scripts/run_L_NB.sh
@@ -10,9 +10,11 @@ do
 
     N_B_I=$(sed -n 10p settings/run_L_NB.yml | tr -d -c 0-9)
     N_B_F=$(sed -n 11p settings/run_L_NB.yml | tr -d -c 0-9)
+    N_FU=$(sed -n 12p settings/run_L_NB.yml | tr -d -c 0-9)
+    N_FD=$(sed -n 13p settings/run_L_NB.yml | tr -d -c 0-9)
 
     for (( N_B=$N_B_I ; N_B<=$N_B_F ; N_B++ ));
     do
-        python code/run_L_NB.py $L $N_B &
+        python code/run_L_NB.py $L $N_FU $N_FD $N_B &
     done
 done
