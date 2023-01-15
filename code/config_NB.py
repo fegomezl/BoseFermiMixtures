87a801d78a6e2987821f4edc94ecf0d66fe1e7d2
Felipe Gómez <fegomezl@unal.edu.co>
2023-01-15 11:13:28 -0500

    added progress bars

--- code/config_NB.py
+++ code/config_NB.py
@@ -23,5 +23,11 @@ if not os.path.exists('results'):
 if not os.path.exists('results/'+foldername):
     os.mkdir('results/'+foldername)
 
+# Crear archivo de progreso
+file = open('results/'+misc.create_foldername(parameters)+'/progress.txt', 'w')
+file.write(str(parameters['N_B_I'])+'\n')
+file.write(str(parameters['N_B_F']))
+file.close()
+
 # Notify telegram
 misc.send_to_telegram('Started '+str(parameters['N_B_F']-parameters['N_B_I']+1)+' jobs:\n'+misc.create_foldername(parameters)+'\nNB: ('+str(parameters['N_B_I'])+','+str(parameters['N_B_F'])+')')
--- code/run_NB.py
+++ code/run_NB.py
@@ -17,5 +17,23 @@ exec('import '+parameters['Type']+' as BFModel')
 # Ejectar simulación
 results = tenpy.run_simulation(**sim_params)
 
-# Notify telegram
-misc.send_to_telegram('Finished: \n'+misc.create_filename(parameters))
+# Actualizar archivo de progreso
+file = open('results/'+misc.create_foldername(parameters)+'/progress.txt', 'a')
+file.write('\n'+str(parameters['N_B']))
+file.close()
+
+# Leer progreso actual
+file = open('results/'+misc.create_foldername(parameters)+'/progress.txt', 'r')
+A = []
+for line in file.readlines():
+    A.append(int(line))
+file.close()
+
+progress = '▢'*(A[1]-A[0]+1)
+for ii in range(2, len(A)):
+    progress = progress[0:A[ii]-A[0]]+'▣'+progress[A[ii]-A[0]+1:]
+if len(A) == A[1]-A[0]+3:
+    progress += '\n Finished: \n'+misc.create_foldername(parameters)
+
+# Enviar notificación
+misc.send_to_telegram('Finished: \n'+misc.create_filename(parameters)+'\n'+progress)
