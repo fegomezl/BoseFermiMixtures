fda58415e34bfadbe114eaa39ff97e7a54440fc5
Felipe Gómez <fegomezl@unal.edu.co>
2023-01-16 14:31:53 -0500

    changed thermodynamic limit parameters

--- code/config_L_NB.py
+++ code/config_L_NB.py
@@ -11,18 +11,13 @@ L = int(sys.argv[1])
 
 # Encontrar puntos mas proximos a la densidad
 # Actualizar número de particulas por densidades
-N_B = int(L*parameters['RHO_B'] + 0.5)
-RHO_B_aux = N_B/L
 
-if RHO_B_aux < parameters['RHO_B']:
-    parameters['N_B_I'] = N_B - parameters['RES_B']
-    parameters['N_B_F'] = N_B + parameters['RES_B']
-elif RHO_B_aux > parameters['RHO_B']:
-    parameters['N_B_I'] = N_B - parameters['RES_B'] - 1
-    parameters['N_B_F'] = N_B + parameters['RES_B'] - 1
-elif RHO_B_aux == parameters['RHO_B']:
-    parameters['N_B_I'] = N_B - parameters['RES_B'] - 1
-    parameters['N_B_F'] = N_B + parameters['RES_B']
+if parameters['EXACT_B'] == 1:
+    parameters['N_B_I'] = int(L*parameters['RHO_B'])-parameters['RES_B']
+    parameters['N_B_F'] = int(L*parameters['RHO_B'])+parameters['RES_B']
+else:
+    parameters['N_B_I'] = int(L*parameters['RHO_B'] + 0.5)-parameters['RES_B']
+    parameters['N_B_F'] = int(L*parameters['RHO_B'] + 0.5)+parameters['RES_B']
 
 parameters['N_FU'] = int(L*parameters['RHO_FU'] + 0.5)
 parameters['N_FD'] = int(L*parameters['RHO_FD'] + 0.5)
--- scripts/run_L_NB.sh
+++ scripts/run_L_NB.sh
@@ -2,16 +2,16 @@
 
 L_I=$(sed -n 3p settings/run_L_NB.yml | tr -d -c 0-9)
 DL=$(sed -n 4p settings/run_L_NB.yml | tr -d -c 0-9)
-L_F=$(sed -n 5p settings/run_L_NB.yml | tr -d -c 0-9)
+NL=$(sed -n 5p settings/run_L_NB.yml | tr -d -c 0-9)
 
-for (( L=$L_I ; L<=$L_F ; L=L+$DL ));
+for (( L=$L_I ; L<=$L_I+$DL*($NL-1) ; L=L+$DL ));
 do
     python code/config_L_NB.py $L
 
-    N_B_I=$(sed -n 10p settings/run_L_NB.yml | tr -d -c 0-9)
-    N_B_F=$(sed -n 11p settings/run_L_NB.yml | tr -d -c 0-9)
-    N_FU=$(sed -n 12p settings/run_L_NB.yml | tr -d -c 0-9)
-    N_FD=$(sed -n 13p settings/run_L_NB.yml | tr -d -c 0-9)
+    N_B_I=$(sed -n 11p settings/run_L_NB.yml | tr -d -c 0-9)
+    N_B_F=$(sed -n 12p settings/run_L_NB.yml | tr -d -c 0-9)
+    N_FU=$(sed -n 13p settings/run_L_NB.yml | tr -d -c 0-9)
+    N_FD=$(sed -n 14p settings/run_L_NB.yml | tr -d -c 0-9)
 
     for (( N_B=$N_B_I ; N_B<=$N_B_F ; N_B++ ));
     do
